4b0c5e8e493cc1da5ab5f4d94dc4706fbbdc631a
L15171 <darkshapeshifter17@gmail.com>
2025-09-12 19:33:43 -0500

    Alternate Intake
    Combined Driving, Intake, and Shooting codes into one. Developed an alternate version for the intake.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveYeetSuck.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "IntakeAndShooter", group = "")
+public class DriveYeetSuck extends LinearOpMode {
+    DcMotor lb;
+    DcMotor rb;
+    DcMotor lf;
+    DcMotor rf;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor intake = hardwareMap.get(DcMotor.class, "intake2");
+        DcMotor shooter = hardwareMap.get(DcMotor.class, "shooterUsingButton");
+
+        lb = hardwareMap.get(DcMotor.class, "lb");
+        rb = hardwareMap.get(DcMotor.class, "rb");
+        lf = hardwareMap.get(DcMotor.class, "lf");
+        rf = hardwareMap.get(DcMotor.class, "rf");
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Motor Power:", intake.getPower());
+            telemetry.addData("Yeeter Power:", shooter.getPower());
+            telemetry.update();
+
+            intake.setPower(-gamepad1.left_stick_y);
+            shooter.setPower(gamepad1.left_trigger * .75);
+            joystickMovement(-gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x, gamepad1.right_stick_y);
+
+
+        }
+    }
+
+
+
+    public void joystickMovement(double lx, double ly, double rx, double ry) {
+        if (rx != 0 || ry != 0) {
+            rf.setPower((-ry + rx) / 2);
+            rb.setPower((-ry + rx) / 2);
+            lf.setPower((ry + rx) / 2);
+            lb.setPower((ry + rx) / 2);
+        } else {
+            rf.setPower((ly + lx) / 2);
+            rb.setPower((ly - lx) / 2);
+            lf.setPower((-ly + lx) / 2);
+            lb.setPower((-ly - lx) / 2);
+        }
+    }
+
+}
