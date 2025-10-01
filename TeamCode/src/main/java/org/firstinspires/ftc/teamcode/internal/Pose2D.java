3485f97b5cc836504fcdcedb2d137f7462ffc8ec
TurtleTekFTC <phillip@footville.org>
2025-09-30 19:27:50 -0500

    Changed Vector2 to Pose2D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internal/Pose2D.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internal/Pose2D.java
@@ -1,21 +1,21 @@
 package org.firstinspires.ftc.teamcode.internal;
 
-public class Vector2 {
+public class Pose2D {
     public double x;
     public double y;
     public double h;
 
-    Vector2(Vector2 vector){
+    Pose2D(Pose2D vector){
         this.x = vector.x;
         this.y = vector.y;
         this.h = vector.h;
     }
-    Vector2(double x, double y, double h){
+    public Pose2D(double x, double y, double h){
         this.x = x;
         this.y = y;
         this.h = h;
     }
-    Vector2(double r, double theta){
+    Pose2D(double r, double theta){
     this.x = r * Math.cos(theta);
     this.y = r * Math.sin(theta);
     }
@@ -23,8 +23,8 @@ public class Vector2 {
         return Math.atan2(y,x);
     }
 
-    public Vector2 add(Vector2 other){
-        return new Vector2(this.x + other.x, this.y + other.y, this.h + other.h);
+    public Pose2D add(Pose2D other){
+        return new Pose2D(this.x + other.x, this.y + other.y, this.h + other.h);
     }
 
 }
