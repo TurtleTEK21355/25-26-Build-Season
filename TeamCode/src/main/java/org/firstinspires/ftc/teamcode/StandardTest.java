b4e7995e210e5fb61c52f05e371b20dd5d457ca9
ByteOfToast <meshirer.noah@gmail.com>
2025-11-24 15:21:37 -0600

    .
    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StandardTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StandardTest.java
@@ -18,13 +18,14 @@ public class StandardTest extends LinearOpMode {
         DcMotor rf = hardwareMap.get(DcMotor.class, hardwareNames.get(HardwareNames.Name.FRONT_RIGHT_MOTOR));
         DcMotor rb = hardwareMap.get(DcMotor.class, hardwareNames.get(HardwareNames.Name.BACK_RIGHT_MOTOR));
         Servo ballGate = hardwareMap.get(Servo.class, hardwareNames.get(HardwareNames.Name.SHOOTER_GATE));
-        lf.setDirection(DcMotorSimple.Direction.REVERSE);
-        lb.setDirection(DcMotorSimple.Direction.REVERSE);
-        ballGate.setDirection(Servo.Direction.REVERSE);
         DcMotor flyWheel = hardwareMap.get(DcMotor.class, hardwareNames.get(HardwareNames.Name.SHOOTER_FLYWHEEL));
         DcMotor intake = hardwareMap.get(DcMotor.class, hardwareNames.get(HardwareNames.Name.INTAKE_MOTOR));
         flyWheel.setDirection(DcMotorSimple.Direction.REVERSE);
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        lf.setDirection(DcMotorSimple.Direction.REVERSE);
+        lb.setDirection(DcMotorSimple.Direction.REVERSE);
+        ballGate.setDirection(Servo.Direction.REVERSE);
+
         double stick;
         ballGate.setPosition((double)1/3); // sets to open position
         waitForStart();
