0515b1e88ec40e58b7185fc016d8f6d3449944db
Peter Turner <petturn@cdw.com>
2025-12-05 18:10:17 -0600

    adding unit test framework for team to hack on

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/internal/DrivetrainTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.internal;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.any;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DrivetrainTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testControlSetsCorrectMotorPower() {
+    }
+
+    @Test
+    public void testControlClipsValuesToRange() {
+    }
+}
