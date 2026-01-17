6d8cf7141ed8798111e88edae9b1e28f1cd88393
Phillip Turner <phillip@footville.org>
2026-01-16 19:12:28 -0600

    Added Start Values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoRedFrontJan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoRedFrontJan.java
@@ -18,7 +18,7 @@ import org.firstinspires.ftc.teamcode.opmode.internal.ShootAutoOpMode;
 @Autonomous(name="AutoRedFrontJan", group="Autonomous")
 public class AutoRedFrontJan extends ShootAutoOpMode {
     private final AllianceSide SIDE = AllianceSide.RED;
-    private final Pose2D STARTING_POSITION = new Pose2D(20, 0, 0);
+    private final Pose2D STARTING_POSITION = new Pose2D(39, 50, -45);
 
 
     int shootWaitTime = 300;
