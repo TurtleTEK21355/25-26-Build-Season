5708056705d84fda43fd60b6789dadf8abbd9449
ByteOfToast <meshirer.noah@gmail.com>
2026-02-01 17:51:32 -0600

    Merge from State Code Part 2
    Resolved conflicts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/shoot/teleop/BackupTeleOp.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode.opmode.shoot.teleop;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.TelemetryPasser;
+<<<<<<< HEAD:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/BackupTeleOp.java
+import org.firstinspires.ftc.teamcode.hardware.HardwareNames;
+import org.firstinspires.ftc.teamcode.hardware.OTOSSensor;
+=======
+import org.firstinspires.ftc.teamcode.subsystems.shoot.ShootHardwareNames;
+import org.firstinspires.ftc.teamcode.subsystems.shared.sensor.OTOSSensor;
+>>>>>>> State-Code:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/shoot/teleop/BackupTeleOp.java
+
+@TeleOp(name = "Backup TeleOp", group="Linear OpModes")
+public class BackupTeleOp extends LinearOpMode {
+    ShootHardwareNames hardwareNames = new ShootHardwareNames();
+    @Override
+    public void runOpMode() {
+        OTOSSensor otosSensor = new OTOSSensor(hardwareMap.get(SparkFunOTOS.class, hardwareNames.get(ShootHardwareNames.Name.ODOMETRY_SENSOR)));
+        otosSensor.configureOtos(0, 0, 0, DistanceUnit.INCH, AngleUnit.DEGREES, 1.0, 1.0);
+        DcMotor lf = hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.FRONT_LEFT_MOTOR));
+        DcMotor lb = hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.BACK_LEFT_MOTOR));
+        DcMotor rf = hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.FRONT_RIGHT_MOTOR));
+        DcMotor rb = hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.BACK_RIGHT_MOTOR));
+        Servo ballGate = hardwareMap.get(Servo.class, hardwareNames.get(ShootHardwareNames.Name.SHOOTER_GATE));
+        DcMotorEx flyWheel = hardwareMap.get(DcMotorEx.class, hardwareNames.get(ShootHardwareNames.Name.SHOOTER_FLYWHEEL));
+        DcMotor intake = hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.INTAKE_MOTOR));
+        flyWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        lf.setDirection(DcMotorSimple.Direction.REVERSE);
+        lb.setDirection(DcMotorSimple.Direction.REVERSE);
+        ballGate.setDirection(Servo.Direction.REVERSE);
+
+        double ly;
+        double lx;
+        double ry;
+        double rx;
+        ballGate.setPosition((double)1/3); // sets to open position
+        waitForStart();
+        flyWheel.setVelocity(1250);
+        while (opModeIsActive()) {
+            rx = gamepad1.right_stick_x;
+            ry = gamepad1.right_stick_y;
+            lx = gamepad1.left_stick_x;
+            ly = -gamepad1.left_stick_y;
+            if (rx != 0 || ry != 0) {
+                double magnitude = Math.hypot(ry, rx);
+                TelemetryPasser.telemetry.addData("magnitude", magnitude);
+                TelemetryPasser.telemetry.update();
+                double fr = (ry + rx) / 2;
+                double br = (ry + rx) / 2;
+                double fl = (ry - rx) / 2;
+                double bl = (ry - rx) / 2;
+                double min = Math.min(Math.min(fr, br), Math.min(fl, bl));
+                rf.setPower((fr/min)*magnitude);
+                rb.setPower((br/min)*magnitude);
+                lb.setPower((fl/min)*magnitude);
+                lb.setPower((bl/min)*magnitude);
+            } else {
+                double magnitude = Math.hypot(ly, lx);
+                TelemetryPasser.telemetry.addData("magnitude", magnitude);
+                TelemetryPasser.telemetry.update();
+                double fr = (ly - lx) / 2;
+                double br = (ly + lx) / 2;
+                double fl = (ly + lx) / 2;
+                double bl = (ly - lx) / 2;
+                double min = Math.min(Math.min(fr, br), Math.min(fl, bl));
+                min = Math.abs(min);
+                rf.setPower((fr/min)*magnitude);
+                rb.setPower((br/min)*magnitude);
+                lf.setPower((fl/min)*magnitude);
+                lb.setPower((bl/min)*magnitude);
+
+            }
+            if(gamepad2.a) {
+                ballGate.setPosition(0); // sets to open position
+            } else {
+                ballGate.setPosition((double) 1 / 3); //sets to closed position
+            }
+            if(gamepad2.left_bumper) {
+                intake.setPower(0.8);
+            } else if(gamepad2.left_trigger > 0.2) {
+                intake.setPower(-0.8);
+            } else {
+                intake.setPower(0);
+            }
+        }
+        flyWheel.setVelocity(0);
+        sleep(750);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/shoot/teleop/TeleOpManualOffset.java
@@ -0,0 +1,123 @@
+package org.firstinspires.ftc.teamcode.opmode.shoot.teleop;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.physicaldata.AllianceSide;
+import org.firstinspires.ftc.teamcode.TelemetryPasser;
+import org.firstinspires.ftc.teamcode.hardware.Ada2167BreakBeam;
+import org.firstinspires.ftc.teamcode.lib.math.Pose2D;
+<<<<<<< HEAD:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpManualOffset.java
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.FlyWheel;
+import org.firstinspires.ftc.teamcode.hardware.HardwareNames;
+import org.firstinspires.ftc.teamcode.subsystems.GateSystem;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.hardware.OTOSSensor;
+import org.firstinspires.ftc.teamcode.subsystems.PartnerPark;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterSystem;
+=======
+import org.firstinspires.ftc.teamcode.subsystems.shared.actuator.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.shared.actuator.FlyWheel;
+import org.firstinspires.ftc.teamcode.subsystems.shoot.ShootHardwareNames;
+import org.firstinspires.ftc.teamcode.subsystems.shoot.GateSystem;
+import org.firstinspires.ftc.teamcode.subsystems.shared.actuator.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.shared.sensor.OTOSSensor;
+import org.firstinspires.ftc.teamcode.subsystems.shoot.actuator.PartnerPark;
+import org.firstinspires.ftc.teamcode.subsystems.shoot.ShooterSystem;
+>>>>>>> State-Code:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/shoot/teleop/TeleOpManualOffset.java
+
+@TeleOp(name="TeleOpManualOffset", group="Iterative OpModes")
+public class TeleOpManualOffset extends OpMode {
+
+    Drivetrain drivetrain;
+    OTOSSensor otosSensor;
+    ShooterSystem shooterSystem;
+    AllianceSide side;
+    Pose2D position;
+    PartnerPark partnerPark;
+    ShootHardwareNames hardwareNames = new ShootHardwareNames();
+    boolean blue = true;
+
+    @Override
+    public void init() {
+        TelemetryPasser.telemetry = telemetry;
+        Object positionObject = blackboard.getOrDefault("Position", new Pose2D(0,0,0));
+        Pose2D position = (Pose2D) positionObject;
+        otosSensor = new OTOSSensor(hardwareMap.get(SparkFunOTOS.class, hardwareNames.get(ShootHardwareNames.Name.ODOMETRY_SENSOR)));
+        otosSensor.configureOtos(position.x, position.y, position.h, DistanceUnit.INCH, AngleUnit.DEGREES, 1.0, 1.0);
+
+        drivetrain = new Drivetrain(
+                hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.FRONT_LEFT_MOTOR)),
+                hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.FRONT_RIGHT_MOTOR)),
+                hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.BACK_LEFT_MOTOR)),
+                hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.BACK_RIGHT_MOTOR)),
+                otosSensor);
+
+        shooterSystem = new ShooterSystem(
+                new FlyWheel(hardwareMap.get(DcMotorEx.class, hardwareNames.get(ShootHardwareNames.Name.SHOOTER_FLYWHEEL))),
+                new GateSystem(
+                        hardwareMap.get(Servo.class, hardwareNames.get(ShootHardwareNames.Name.SHOOTER_GATE)),
+                        hardwareMap.get(Ada2167BreakBeam.class, hardwareNames.get(ShootHardwareNames.Name.BALL_READY_SENSOR))),
+                new Intake(hardwareMap.get(DcMotor.class, hardwareNames.get(ShootHardwareNames.Name.INTAKE_MOTOR))), side);
+//        partnerPark = new PartnerPark(
+//                hardwareMap.get(DcMotor.class, "vsr"),
+//                hardwareMap.get(DcMotor.class, "vsl"));
+
+    }
+
+    @Override
+    public void loop() {
+        if (blue) {
+            drivetrain.fcControl(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, 0);
+        } else {
+            drivetrain.fcControl(-gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, 0);
+        }
+        if (gamepad1.back){
+            otosSensor.resetPosition();
+        }
+        setOffsetPosition();
+        setAlliance();
+        shooterSystem.teleOpControl(otosSensor.getPosition(), gamepad2.left_bumper,gamepad2.right_bumper, gamepad2.left_trigger, gamepad1.a, gamepad1.b);
+//        partnerPark.control(gamepad1.right_bumper, gamepad1.left_bumper);
+        telemetry.addData("hpos:", otosSensor.getPosition().h);
+        drivetrain.powerTelemetry();
+        telemetry.update();
+
+    }
+    public void setAlliance() {
+        if (gamepad2.dpad_right) {
+            blue = true;
+        } else if (gamepad2.dpad_left) {
+            blue = false;
+        }
+    }
+    public void setOffsetPosition() {
+        if (gamepad1.dpad_up) {
+            if(blue) {
+                otosSensor.setPosition(new Pose2D(-20, 58, 0));
+            } else {
+                otosSensor.setPosition(new Pose2D(20, 58, 0));
+            }
+        } else if (gamepad1.dpad_down) {
+            if(blue) {
+                otosSensor.setPosition(new Pose2D(-16, -24, 0));
+            } else {
+                otosSensor.setPosition(new Pose2D(16, -24, 0));
+            }
+        } else if (gamepad1.dpad_left) {
+            if(blue) {
+                otosSensor.setPosition(new Pose2D(-15, -61, 0));
+            } else {
+                otosSensor.setPosition(new Pose2D(15, -61, 0));
+            }
+        }
+    }
+
+}
