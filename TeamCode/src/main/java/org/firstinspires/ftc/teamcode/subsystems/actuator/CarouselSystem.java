1e854ffb9962c58a9c0c77916a20b10149d0fed2
Phillip Turner <phillip@footville.org>
2026-02-17 19:25:03 -0600

    updated carouselsystem again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/actuator/CarouselSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/actuator/CarouselSystem.java
@@ -12,7 +12,7 @@ public class CarouselSystem {
     private Servo carouselServo;
     private ColorSensorArray colorSensorArray;
     private final Double[] slotPositions = {0.0, 0.4, 0.8};
-    boolean movingToSlot = false;
+    private final double servoPositionTolerance = 0.05;
 
     public CarouselSystem(Servo carouselServo, ColorSensorArray colorSensorArray) {
         this.carouselServo = carouselServo;
@@ -31,8 +31,8 @@ public class CarouselSystem {
 
     private boolean getSlotIsInShoot(int slot, double position) {
         //todo add constants And check slot between 0 an 2
-        return (slotPositions[slot] + 0.05) > position && position > (slotPositions[slot] - 0.05);
-
+        return (slotPositions[slot] + servoPositionTolerance) > position &&
+                position > (slotPositions[slot] - servoPositionTolerance);
     }
 
     public int getSlotInShoot() {
@@ -50,20 +50,14 @@ public class CarouselSystem {
     }
 
     public void setSlotInShoot(int slot) {
-        movingToSlot = true;
         setPosition(slotPositions[slot]);
     }
 
     public void setArtifactToShoot(ArtifactState state) {
         int slot = getSlotInShoot();
         // don't set to a new position while  servo is in motion
-        if (!movingToSlot && colorSensorArray.getArtifactState(ColorSensorPosition.SHOOT) != state) {
-            //slot is -1 and not moving if in initial state
-            if (slot == -1) {
-                //move to 0 by default
-                //todo make this an initialize function
-                setSlotInShoot(0);
-            } else if (slot == 0) {
+        if (colorSensorArray.getArtifactState(ColorSensorPosition.SHOOT) != state) {
+            if (slot == 0) {
                 //the artifact in the left color sensor is the same as the color of the artifact we are looking for
                 //and the current slot is 0
                 if (colorSensorArray.getArtifactState(ColorSensorPosition.LEFT) == state) {
@@ -95,18 +89,13 @@ public class CarouselSystem {
                 if (colorSensorArray.getArtifactState(ColorSensorPosition.RIGHT) == state) {
                     // move from slot
                     setSlotInShoot(1);//one is closer to two then 0 is
-                //the artifact in the left color sensor is the same as the color of the artifact we are looking for
-                //and the current slot is 2
+                    //the artifact in the left color sensor is the same as the color of the artifact we are looking for
+                    //and the current slot is 2
                 } else if (colorSensorArray.getArtifactState(ColorSensorPosition.LEFT) == state) {
                     // move from slot 2 to slot 0
                     setSlotInShoot(0);
                 }
             }
-        }
-        //the servo position is 0,1 or 2 and we don't have to move the carousel so set movingToSlot to false
-        //TODO check for what you ARE looking for, not what you are NOT looking for
-        else if (slot != -1) {
-            movingToSlot = false;
         }
     }
 }
