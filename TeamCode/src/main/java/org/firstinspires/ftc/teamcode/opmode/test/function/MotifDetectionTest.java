4c7a7a71fd8bc666a35302020fd44384993a3b61
ByteOfToast <meshirer.noah@gmail.com>
2026-02-24 17:16:39 -0600

    Fixed angle of Motif test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/function/MotifDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/function/MotifDetectionTest.java
@@ -32,7 +32,7 @@ public class MotifDetectionTest extends StateAutoOpMode {
     @Override
     public void commands() {
         addCommand(new SimultaneousAnyCommand(new GetMotifCommand(robot.getLimelight()), new TimerCommand(3000)));
-        addCommand(new MovePIDHoldTimeCommand(new Pose2D(0, 0, 180), 2000, SPEED, robot.getDrivetrain(), robot.getOtosSensor()));
+        addCommand(new MovePIDHoldTimeCommand(new Pose2D(0, 0, 0), 2000, SPEED, robot.getDrivetrain(), robot.getOtosSensor()));
     }
 
     public void dataHandler(){
