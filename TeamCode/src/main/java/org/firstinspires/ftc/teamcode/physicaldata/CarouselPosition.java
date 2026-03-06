1251f03fb718f09ae9a2b5f3e98a150400de3dd0
Peter Turner <pturner@uwalumni.com>
2026-03-05 18:00:45 -0600

    Refactor CarouselPosition to use DoubleSupplier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/physicaldata/CarouselPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/physicaldata/CarouselPosition.java
@@ -1,29 +1,36 @@
 package org.firstinspires.ftc.teamcode.physicaldata;
 
 import org.firstinspires.ftc.teamcode.Constants;
-import org.firstinspires.ftc.teamcode.subsystems.actuator.CarouselSystem;
+
+import java.util.function.DoubleSupplier;
 
 public enum CarouselPosition {
-    UNSET(Double.NaN, false, false),
-    SHOOT_SLOT_0(Constants.shootSlot0,false, true), //slot 0 is the slot closest to servo position 0
-    SHOOT_SLOT_1(Constants.shootSlot1,false, true),
-    SHOOT_SLOT_2(Constants.shootSlot2,false, true),
-    INTAKE_SLOT_0(Constants.intakeSlot0,true, false),
-    INTAKE_SLOT_1(Constants.intakeSlot1,true, false),
-    INTAKE_SLOT_2(Constants.intakeSlot2,true, false);
+    UNSET(() -> Double.NaN, false, false),
 
-    private final double servoPosition;
+    // slot 0 is the slot closest to servo position 0
+    SHOOT_SLOT_0(() -> Constants.shootSlot0, false, true),
+    SHOOT_SLOT_1(() -> Constants.shootSlot1, false, true),
+    SHOOT_SLOT_2(() -> Constants.shootSlot2, false, true),
+
+    INTAKE_SLOT_0(() -> Constants.intakeSlot0, true, false),
+    INTAKE_SLOT_1(() -> Constants.intakeSlot1, true, false),
+    INTAKE_SLOT_2(() -> Constants.intakeSlot2, true, false);
+
+    private final DoubleSupplier servoPositionSupplier;
     private final boolean canIntake;
     private final boolean canShoot;
 
-    CarouselPosition(double servoPosition, boolean canIntake, boolean canShoot) {
-        this.servoPosition = servoPosition;
+    CarouselPosition(DoubleSupplier servoPositionSupplier, boolean canIntake, boolean canShoot) {
+        this.servoPositionSupplier = servoPositionSupplier;
         this.canIntake = canIntake;
         this.canShoot = canShoot;
     }
 
+    /**
+     * IMPORTANT: This now returns the CURRENT @Config value every time it's called.
+     */
     public double getServoPosition() {
-        return servoPosition;
+        return servoPositionSupplier.getAsDouble();
     }
 
     public boolean getCanShoot() {
@@ -38,7 +45,7 @@ public enum CarouselPosition {
         switch(position) {
             case UNSET:
             case INTAKE_SLOT_0:
-                return SHOOT_SLOT_0; //if you are unset, go to slot 0
+                return SHOOT_SLOT_0; // if you are unset, go to slot 0
             case SHOOT_SLOT_0:
             case INTAKE_SLOT_1:
                 return SHOOT_SLOT_1;
