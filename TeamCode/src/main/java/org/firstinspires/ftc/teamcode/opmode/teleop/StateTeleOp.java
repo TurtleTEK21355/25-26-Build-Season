8089a6838b0efd06316f80781a24bcdec53f414f
Phillip Turner <phillip@footville.org>
2026-03-10 19:21:02 -0500

    added disabling all parts of teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/StateTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/StateTeleOp.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
-import com.qualcomm.hardware.limelightvision.LLResult;
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -11,20 +11,16 @@ import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.TelemetryPasser;
 import org.firstinspires.ftc.teamcode.commands.NextShootCommand;
 import org.firstinspires.ftc.teamcode.commands.PreviousShootCommand;
-import org.firstinspires.ftc.teamcode.commands.SelectArtifactCommand;
-import org.firstinspires.ftc.teamcode.commands.SetCarouselPositionCommand;
 import org.firstinspires.ftc.teamcode.commands.ShootAllArtifactsCommand;
-import org.firstinspires.ftc.teamcode.commands.TimerCommand;
 import org.firstinspires.ftc.teamcode.lib.command.CommandScheduler;
 import org.firstinspires.ftc.teamcode.lib.math.Pose2D;
 import org.firstinspires.ftc.teamcode.lib.pid.PIDControllerHeading;
-import org.firstinspires.ftc.teamcode.lib.pid.PIDControllerSpeedLimit;
 import org.firstinspires.ftc.teamcode.physicaldata.AllianceSide;
 import org.firstinspires.ftc.teamcode.physicaldata.CarouselPosition;
 import org.firstinspires.ftc.teamcode.physicaldata.Motif;
 import org.firstinspires.ftc.teamcode.subsystems.StateRobot;
-import org.firstinspires.ftc.teamcode.subsystems.actuator.ShooterSystem;
 
+@Configurable
 @TeleOp(name = "TeleOp", group = "teleop")
 public class StateTeleOp extends OpMode {
     protected StateRobot robot;
@@ -35,12 +31,21 @@ public class StateTeleOp extends OpMode {
     private int velocity = Constants.shootCloseVelocity;
     private double angle = Constants.shootCloseAngle;
 
-    private final PIDControllerHeading hPID = new PIDControllerHeading(Constants.getAngularPIDConstants(), Constants.cameraAngleOffset, Constants.getPIDTolerance().h, Constants.blindRotateSpeed);
     private CarouselPosition firstShot = CarouselPosition.UNSET;
     private int shotCount = 0;
 
-    private boolean artifactStateShootEnabled = false;
+    private Pose2D position = new Pose2D();
+    private final PIDControllerHeading hPID = new PIDControllerHeading(Constants.getAngularPIDConstants(), Constants.cameraAngleOffset, Constants.getPIDTolerance().h, Constants.blindRotateSpeed);
+
     private Motif motif = Motif.NONE;
+
+    public static boolean stateShootCommandUpdating = false;
+    public static boolean positionUpdating = true;
+    public static boolean turretUpdating = true;
+    public static boolean intakeUpdating = true;
+    public static boolean rotateToGoalUpdating = true;
+    public static boolean shootingCommandUpdating = true;
+    public static boolean carouselMoveUpdating = true;
 
     @Override
     public void init() {
@@ -89,61 +94,67 @@ public class StateTeleOp extends OpMode {
         boolean cancelShoot = gamepad2.back && gamepad2.start;
 
 
-
-        if (resetPositionButton) {
-            robot.getOtosSensor().resetPosition();
-            robot.getIMU().resetYaw();
-        }
-        double robotYaw = robot.getIMU().getRobotYawPitchRollAngles().getYaw();
-        robot.getLimelight().updateRobotOrientation(robotYaw); //gets proper position
-        Pose2D limelightPosition = robot.getLimelight().getPositionFromGoal();
-        Pose2D position = new Pose2D(limelightPosition.x, limelightPosition.y, robotYaw);
-        telemetry.addData("Position", position);
-        telemetry.addData("IMU Yaw: ", robot.getIMU().getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
-
-
-        velocity += (int) (velocityTweakStick*delta.seconds()*150); //inverted bc of gamepad
-        angle += angleTweakStick*delta.seconds()*3;
-        delta.reset();
-
-        if (closeShootPresetButton) {
-            angle = Constants.shootCloseAngle;
-            velocity = Constants.shootCloseVelocity;
-        }
-        if (farShootPresetButton) {
-            angle = Constants.shootFarAngle;
-            velocity = Constants.shootFarVelocity;
-        }
-        if (stopFlywheelButton) {
-            velocity = 0;
+        if (positionUpdating) {
+            if (resetPositionButton) {
+                robot.getOtosSensor().resetPosition();
+                robot.getIMU().resetYaw();
+            }
+            double robotYaw = robot.getIMU().getRobotYawPitchRollAngles().getYaw();
+            robot.getLimelight().updateRobotOrientation(robotYaw); //gets proper position
+            Pose2D limelightPosition = robot.getLimelight().getPositionFromGoal();
+            position = new Pose2D(limelightPosition.x, limelightPosition.y, robotYaw);
+            telemetry.addData("Position", position);
+            telemetry.addData("IMU Yaw: ", robot.getIMU().getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
         }
 
-        velocity = Range.clip(velocity, 0, 1500);
-        angle = Range.clip(angle, 25, 40);
-        robot.getShooterSystem().setFlywheelVelocity(velocity);
-        robot.getShooterSystem().setHoodAngle(angle);
-        telemetry.addData("Velocity (Expected/Actual)", "%d/%.2f", velocity, robot.getShooterSystem().getFlywheelVelocity());
-        telemetry.addData("Angle", "%.2f", angle);
-        telemetry.addData("Lifter Position", robot.getShooterSystem().getArtifactLift().getLiftPosition());
-        telemetry.addData("Carousel Position", robot.getShooterSystem().getCarouselPosition().name());
-        
-        robot.getShooterSystem().setIntakePower(Range.clip(intakePowerAxis, -1, 1));
+        if (turretUpdating) {
+            velocity += (int) (velocityTweakStick * delta.seconds() * 150);
+            angle += angleTweakStick * delta.seconds() * 3;
+            delta.reset();
 
-        if (rotateToGoalButton) {
-            if (robot.getLimelight().isDetectingGoal(robot.getAllianceSide())) {
-                double currentAngleError = robot.getLimelight().getAngleFromGoal();
-                if (!hPID.atTarget(currentAngleError)) {
-                    robot.getDrivetrain().control(0.0, 0.0, hPID.calculate(currentAngleError));
+            if (closeShootPresetButton) {
+                angle = Constants.shootCloseAngle;
+                velocity = Constants.shootCloseVelocity;
+            }
+            if (farShootPresetButton) {
+                angle = Constants.shootFarAngle;
+                velocity = Constants.shootFarVelocity;
+            }
+            if (stopFlywheelButton) {
+                velocity = 0;
+            }
+
+            velocity = Range.clip(velocity, 0, 1500);
+            angle = Range.clip(angle, 25, 40);
+            robot.getShooterSystem().setFlywheelVelocity(velocity);
+            robot.getShooterSystem().setHoodAngle(angle);
+            telemetry.addData("Velocity (Expected/Actual)", "%d/%.2f", velocity, robot.getShooterSystem().getFlywheelVelocity());
+            telemetry.addData("Angle", "%.2f", angle);
+            telemetry.addData("Lifter Position", robot.getShooterSystem().getArtifactLift().getLiftPosition());
+            telemetry.addData("Carousel Position", robot.getShooterSystem().getCarouselPosition().name());
+        }
+
+        if (intakeUpdating) {
+            robot.getShooterSystem().setIntakePower(Range.clip(intakePowerAxis, -1, 1));
+        }
+
+        if (rotateToGoalUpdating) {
+            if (rotateToGoalButton) {
+                if (robot.getLimelight().isDetectingGoal(robot.getAllianceSide())) {
+                    double currentAngleError = robot.getLimelight().getAngleFromGoal();
+                    if (!hPID.atTarget(currentAngleError)) {
+                        robot.getDrivetrain().control(0.0, 0.0, hPID.calculate(currentAngleError));
+                    }
+                } else {
+                    robot.getDrivetrain().control(0, 0, rotateStick);
                 }
             } else {
-                robot.getDrivetrain().control(0,0,rotateStick);
+                double speedFactor = 1 - Range.clip(speedFactorAxis, 0, 0.5);
+                robot.getDrivetrain().fcControl(forwardBackwardsStick * speedFactor, strafeStick * speedFactor, rotateStick * speedFactor, position.h);
             }
-        } else {
-            double speedFactor = 1 - Range.clip(speedFactorAxis, 0, 0.5);
-            robot.getDrivetrain().fcControl(forwardBackwardsStick * speedFactor, strafeStick * speedFactor, rotateStick * speedFactor, robot.getIMU().getRobotYawPitchRollAngles().getYaw());
         }
 
-        if (artifactStateShootEnabled) {
+        if (stateShootCommandUpdating) {
             //motif override for testing
             if (gppMotif) {
                 motif = Motif.GPP;
@@ -168,27 +179,28 @@ public class StateTeleOp extends OpMode {
             }
         }
 
-        if (gamepad2.rightBumperWasPressed()) {
-            if (commandScheduler.isCompleted()) {
-                firstShot = robot.getShooterSystem().getCarouselPosition();
-            }
+        if (shootingCommandUpdating) {
+            if (gamepad2.rightBumperWasPressed()) {
+                if (commandScheduler.isCompleted()) {
+                    firstShot = robot.getShooterSystem().getCarouselPosition();
+                }
 
-            if (firstShot == CarouselPosition.INTAKE_SLOT_0 && shotCount <= 2) {
-                commandScheduler.add(new NextShootCommand(robot.getShooterSystem()));
-                shotCount++;
-            }
-            else if (firstShot == CarouselPosition.INTAKE_SLOT_1 && shotCount <=1) {
-                commandScheduler.add(new PreviousShootCommand(robot.getShooterSystem()));
-                shotCount++;
-            }
-            else if (firstShot == CarouselPosition.INTAKE_SLOT_2 && shotCount <= 2){
-                commandScheduler.add(new PreviousShootCommand(robot.getShooterSystem()));
-                shotCount++;
+                if (firstShot == CarouselPosition.INTAKE_SLOT_0 && shotCount <= 2) {
+                    commandScheduler.add(new NextShootCommand(robot.getShooterSystem()));
+                    shotCount++;
+                } else if (firstShot == CarouselPosition.INTAKE_SLOT_1 && shotCount <= 1) {
+                    commandScheduler.add(new PreviousShootCommand(robot.getShooterSystem()));
+                    shotCount++;
+                } else if (firstShot == CarouselPosition.INTAKE_SLOT_2 && shotCount <= 2) {
+                    commandScheduler.add(new PreviousShootCommand(robot.getShooterSystem()));
+                    shotCount++;
+                }
             }
         }
 
         if (cancelShoot) {
             commandScheduler.emptyAll();
+            robot.getShooterSystem().getArtifactLift().setLiftDownNoLimit();
         }
 
         commandScheduler.loop();
@@ -205,16 +217,17 @@ public class StateTeleOp extends OpMode {
             }
             shotCount = 0;
 
-            if (gamepad2.dpadLeftWasPressed()) {
-                robot.getShooterSystem().getCarouselSystem().goToPreviousIntakePosition();
-            }
-            else if (gamepad2.dpadRightWasPressed()) {
-                robot.getShooterSystem().getCarouselSystem().goToNextIntakePosition();
+            if (carouselMoveUpdating) {
+                if (gamepad2.dpadLeftWasPressed()) {
+                    robot.getShooterSystem().getCarouselSystem().goToPreviousIntakePosition();
+                } else if (gamepad2.dpadRightWasPressed()) {
+                    robot.getShooterSystem().getCarouselSystem().goToNextIntakePosition();
+                }
             }
         }
 
-
         telemetry.update();
+
     }
 
 
