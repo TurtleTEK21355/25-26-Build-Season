118703f5ede854790e74737dc8ab6cf78d2b80a8
Peter Turner <pturner@uwalumni.com>
2026-03-13 10:25:20 -0500

    Create AutoAudienceSideBlue.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoAudienceSideBlue.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="AutoAudienceSideBlue", group = "auto")
+public class AutoAudienceSideBlue extends AutoAudienceSide {
+
+    @Override
+    protected void configureSide() {
+
+        START_H = 0.0;
+        SHOOT_H = 33.0;
+        INTAKE_H = 0.0;// not used
+
+        START_Y = -60.0;
+
+        SHOOT_X = 0.0; //not used
+        SHOOT_Y = -50.0;
+
+        INTAKE_1_X = 0.0;// not used
+        INTAKE_2_X = 0.0;//not used
+        INTAKE_3_X = 0.0;//not used
+    }
+}
