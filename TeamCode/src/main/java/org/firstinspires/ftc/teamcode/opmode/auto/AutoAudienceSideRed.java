3df0bea40b3dee6753a1ad5a6216fdce0dbbb47a
Peter Turner <pturner@uwalumni.com>
2026-03-13 19:54:23 -0500

    Update AutoAudienceSideRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoAudienceSideRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoAudienceSideRed.java
@@ -24,20 +24,18 @@ public class AutoAudienceSideRed extends AutoAudienceSide {
     @Override
     protected void configureSide() {
 
-        START_H = 0.0;
-        SHOOT_H = -33.0;
-        INTAKE_H = 0.0;// not used
+        START_H = ar_START_H;
+        SHOOT_H = ar_SHOOT_H;
+        INTAKE_H = ar_INTAKE_H;
 
-        START_Y = -60.0;
+        START_Y = ar_START_Y;
 
-        SHOOT_X = -12.0;
-        SHOOT_Y = -50.0;
-        
-        ROW_2_Y = -12.0;
-        ROW_3_Y = -35.0;
-
-        INTAKE_1_X = 0.0;// not used
-        INTAKE_2_X = 0.0;//not used
-        INTAKE_3_X = 0.0;//not used
+        SHOOT_X = ar_SHOOT_X;
+        SHOOT_Y = ar_SHOOT_Y;
+        ROW_2_Y = ar_ROW_2_Y;
+        ROW_3_Y = ar_ROW_3_Y;
+        INTAKE_1_X = ar_INTAKE_1_X;
+        INTAKE_2_X = ar_INTAKE_2_X;
+        INTAKE_3_X = ar_INTAKE_3_X;
     }
 }
