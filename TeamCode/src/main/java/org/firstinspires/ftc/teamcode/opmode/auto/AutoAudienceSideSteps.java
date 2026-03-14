e6122d10a0eae78b7f0c4e18e87a81f3b2031ef5
Peter Turner <pturner@uwalumni.com>
2026-03-13 20:25:43 -0500

    Update AutoAudienceSideSteps.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoAudienceSideSteps.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoAudienceSideSteps.java
@@ -1,5 +1,19 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
+
+import org.firstinspires.ftc.teamcode.commands.Command;
+import org.firstinspires.ftc.teamcode.commands.MovePIDEncoderCommand;
+import org.firstinspires.ftc.teamcode.commands.RotatePIDCommand;
+import org.firstinspires.ftc.teamcode.commands.Shoot3Command;
+import org.firstinspires.ftc.teamcode.commands.TimerCommand;
+import org.firstinspires.ftc.teamcode.commands.SetIntakePowerCommand;
+import org.firstinspires.ftc.teamcode.commands.SetCarouselPositionCommand;
+import org.firstinspires.ftc.teamcode.commands.GetMotifCommand;
+
+import org.firstinspires.ftc.teamcode.physicaldata.CarouselPosition;
+import org.firstinspires.ftc.teamcode.robot.StateRobot;
+
+
 public enum AutoAudienceSideSteps {
     MOVE_TO_POS_1,
     ROTATE_ROBOT_2,
